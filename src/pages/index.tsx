27186c0ea2746306589efd2ad16aeb4b10fd7546
Kirill Sofronov <2138870+sokirill@users.noreply.github.com>
2022-06-10 21:52:37 +0800

    Setup paths in tsconfig

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,11 @@
-import type { NextPage } from "next"
+import type { NextPage } from 'next'
 
 const Home: NextPage = () => {
-	return <div className='flex flex-col justify-center items-center align-middle h-screen bg-black '>
-		<h1 className='text-4xl text-gray-200'>Hello Next.js</h1>
-	</div>
+  return (
+    <div className='flex h-screen flex-col items-center justify-center bg-black align-middle '>
+      <h1 className='text-4xl text-gray-200'>Hello Next.js</h1>
+    </div>
+  )
 }
 
 export default Home
